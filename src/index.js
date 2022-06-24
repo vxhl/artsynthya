750c2ea3da9d73e01d443d4b482c809e05b5ec37
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2022-06-24 14:01:38 +0530

    adding changes to index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// Our app will look the way we want it to look based on how our renders in
+// index.js look 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+    </Router>
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
